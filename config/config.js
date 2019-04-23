1d56fe2a03c2990d8308931ba1c100aa8d9e0c34
kevin-CodeCrew <kevin@code-crew.org>
2019-04-23 06:12:33 -0500

    checkpoint

---
+++ config/config.js
@@ -0,0 +1,7 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: 'mongodb+srv://cs_test:P1ng\@P0ng\@cluster0-mhpvz.gcp.mongodb.net/cs_project3_mern?retryWrites=true',
+  db_dev: 'mongodb+srv://cs_test:P1ng\@P0ng\@cluster0-mhpvz.gcp.mongodb.net/cs_project3_mern?retryWrites=true',
+  salty_dog: 'st1nk@Bombs',
+};
