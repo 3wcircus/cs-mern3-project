6faa799e1b1801321f8c68bbed67b37aea23f38f
kevin-CodeCrew <kevin@code-crew.org>
2019-04-23 07:42:35 -0500

    check

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {HashRouter as Router, Route, Link, NavLink} from 'react-router-dom';
+import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React I guess
-          </a>
-        </header>
-      </div>
-    );
-  }
+    // constructor to save state (component variables) like isLoggedIn, username, and email
+    constructor(props) {
+        super(props);
+        this.state = {
+            // isLoggedIn is false because a user is not logged in and there is no username or email so they're null
+            isLoggedIn: false,
+            username: null,
+            email: null,
+        };
+    }
+
+    // Changes the state (components variables). It's usually called in child components to change the state in this parent component
+    // The parameters in here will be replaced with a username, email, or true/false if a person is going to be logged in
+    loggedInUserInfo = (username, email, loggedIn) => {
+        console.log("Clear");
+        this.setState({
+            username: username,
+            email: email,
+            isLoggedIn: loggedIn,
+        });
+    };
+
+    render() {
+        if (this.state.isLoggedIn) {
+            return (
+                <Router basename="/react-auth-ui/">
+                    <div className="App">
+                        <div className="App__Aside"></div>
+                        <div className="PageSwitcher">
+                            <NavLink to="/sign-out" activeClassName="PageSwitcher__Item--Active"
+                                     className="PageSwitcher__Item">Sign Out</NavLink>
+
+                        </div>
+
+                    </div>
+                </Router>
+            )
+        } else {
+            return (
+                <Router basename="/react-auth-ui/">
+                    <div className="App">
+                        <div className="App__Aside"></div>
+                        <div className="App__Form">
+                            <div className="PageSwitcher">
+                                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active"
+                                         className="PageSwitcher__Item">Sign In</NavLink>
+                                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active"
+                                         className="PageSwitcher__Item">Sign Up</NavLink>
+                            </div>
+
+                            <div className="FormTitle">
+                                <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active"
+                                         className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/"
+                                                                                                   activeClassName="FormTitle__Link--Active"
+                                                                                                   className="FormTitle__Link">Sign
+                                Up</NavLink>
+                            </div>
+
+                            <Route exact path="/" component={SignUpForm}>
+                            </Route>
+                            <Route exact path={"/sign-in"}
+                                   component={() => <SignInForm isLoggedIn={this.state.isLoggedIn}
+                                                                username={this.state.username} email={this.state.email}
+                                                                loggedInUserInfo={this.loggedInUserInfo}/>}/>
+                        </div>
+
+                    </div>
+                </Router>
+            )
+        }
+
+
+    }
 }
 
 export default App;
---
+++ client/src/components/SignInForm.js
@@ -0,0 +1,94 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class SignInForm extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            username: '',
+            password: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange = (e) => {
+        let target = e.target;
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        let name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('/users/login',
+        {
+            method: 'POST',
+            // Accept tells the server what kind of data the client is expecting.
+            // Content-Type tells the server which kind of data the client is sending in the body
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            // Creates a collection for username and password. Because a request can't send a collection, you have to make it a JSON string first
+            // e.target is the information being sent from the form input fields by their names give in the input attributes. The value is what was typed.
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password,
+            }),
+        })
+    // data on the left side is the raw response data the server sent (res.send)
+    // On the right side, use that data parameter with the .json function to change data to a readable JSON collection.
+        .then(rawData => rawData.json())
+        // response on the left side is the readable JSON collection.
+        // on the right side we're running a function. The first line is console logging
+        .then(userAndEmail => {
+            console.log(userAndEmail);
+            // If the server (res.send) has a collection with a username in it, run the function below
+            if (userAndEmail.username)
+            // This is changing the parent component state to the returned username, returned email and isLoggedIn to true
+                return this.props.loggedInUserInfo(userAndEmail.username, userAndEmail.email, true);
+            // If the server (res.send) DOES NOT have username in it, run the function below
+            else
+            // This is changing the parent component state to have the username and password to be null (empty) and isLoggedIn to false
+                return this.props.loggedInUserInfo(null, null, false)
+        });
+        console.log('The form was submitted with the following data:');
+        console.log(this.state);
+    };
+
+
+    render() {
+        return (
+            <div className="FormCenter">
+                <form className="FormFields" onSubmit={this.handleSubmit}>
+                    <div className="FormField">
+                        <label className="FormField__Label" htmlFor="username">User Name</label>
+                        <input type="text" id="username" className="FormField__Input" placeholder="Enter your user name"
+                               name="username" value={this.state.username} onChange={this.handleChange}/>
+                    </div>
+
+                    <div className="FormField">
+                        <label className="FormField__Label" htmlFor="password">Password</label>
+                        <input type="password" id="password" className="FormField__Input"
+                               placeholder="Enter your password" name="password" value={this.state.password}
+                               onChange={this.handleChange}/>
+                    </div>
+
+                    <div className="FormField">
+                        <button className="FormField__Button mr-20">Sign In</button>
+                        <Link to="/" className="FormField__Link">Create an account</Link>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignInForm;
